da1cb20f6d45869a611ca22c8d42abe4479c5d06
Ritish-06 <ritishsundaresan@gmail.com>
2026-09-11 11:22:38 +0530

    Add Add Two Numbers solution

--- Arrays/ValidParanthesis.java
+++ Arrays/ValidParanthesis.java
@@ -1,6 +1,6 @@
 class Solution {
     public boolean isValid(String s) {
-        Stack<Character> stack = new Stack<>();
+        Stack<Character> stack = new Stac   k<>();
 
         for (char c : s.toCharArray()) {
             if (c == '(')
